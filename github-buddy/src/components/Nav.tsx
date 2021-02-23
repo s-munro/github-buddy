c0897740554b2b3ef26698bb72ccc5ef5d794371
s-munro <68410363+s-munro@users.noreply.github.com>
2021-02-23 00:01:01 -0500

    chore: nav changes depending on token in localStorage

--- github-buddy/src/components/Nav.tsx
+++ github-buddy/src/components/Nav.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Nav = () => {
-  return (
+  return localStorage.getItem("token") ? (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link onClick={() => localStorage.removeItem("token")} to="/login">
+        Log Out
+      </Link>
+    </nav>
+  ) : (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/signup">Signup</Link>
